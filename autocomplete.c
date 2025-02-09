7649d1120cdc91d5a5f3ce1d1da87b5f44135b26
chrismdemian <chrismdemian@gmail.com>
2025-02-09 01:43:40 -0500

    Initial commit

---
+++ autocomplete.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void read_in_terms(term **terms, int *pnterms, char *filename); {
+
+// Takes in a pointer to a term, pointer to a number of terms, and a pointer to a txt file
+// 
+
+}
